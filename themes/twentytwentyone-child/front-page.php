67e6b187f1e5154f64647abf5a159e7b6a8e6465
Connor Theriault <connor.theriault@dcmail.ca>
2021-12-05 13:43:30 -0500

    adding some starting code

---
+++ themes/twentytwentyone-child/front-page.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Template Name: Front Page Template
+
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header(); ?>
+
+<div class="content-area" id="primaru">
+    <main id="main" class="site-main" role="main">
+        <p>content area</p>
+    </main>
+</div>
+
+
+<?php
+get_footer()
+
+?>
